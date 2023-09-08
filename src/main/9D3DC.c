92dae336800a9b0132eb36f44a362ad26b960312
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-09-08 13:24:18 -0300

    File Splits

---
+++ src/main/9D3DC.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/main/nonmatchings/9D3DC", func_800BE880);
+
+INCLUDE_ASM("asm/main/nonmatchings/9D3DC", func_800BE8B0);
---
+++ src/main/9D4CC.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/main/nonmatchings/9D4CC", func_800BE970);
+
+INCLUDE_ASM("asm/main/nonmatchings/9D4CC", func_800BE998);
+
+INCLUDE_ASM("asm/main/nonmatchings/9D4CC", func_800BE9B0);
