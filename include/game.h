935fe2e55e559846895351c283f7842a93b5b8c2
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-09-14 07:13:46 -0300

    g_CurrentEntity allocation comment

--- include/game.h
+++ include/game.h
@@ -7,4 +7,8 @@ typedef struct Entity {
     /* 0xA4 */ s32 unkA4;
 } Entity;
 
+/*
+ * g_CurrentEntity is allocated in 0x1F8000A4
+ * which is part of the PSX D-Cache area.
+*/
 extern Entity* g_CurrentEntity;
