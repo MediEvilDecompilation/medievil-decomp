52bc7159ce8d857a4b0ed81f7c474d3aa07560bc
NedEX <nedex@users.noreply.github.com>
2023-09-21 00:03:07 +0200

    Update include/game.h
    Co-authored-by: Alejandro Asenjo Nitti <96613413+sonicdcer@users.noreply.github.com>

--- include/game.h
+++ include/game.h
@@ -23,7 +23,7 @@ extern s32 D_800F6D08;
 extern void func_80059CCC();
 extern void func_80059E58();
 extern void func_80099AFC();
-extern s32 D_800CDF5C;
+extern s32 D_800CDF5C[];
 extern s32 D_800EE020;
 extern u16 D_800EEE5E;
 extern s32 D_800EEE64;
