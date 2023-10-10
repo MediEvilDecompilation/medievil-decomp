56381b6112c75caa83ce28c0d4c7a01434317f46
DerpPrincess <AllisonMackenzieJohnson@gmail.com>
2023-10-09 22:05:48 -0500

    formatting

--- src/ovl/credits/DD8.c
+++ src/ovl/credits/DD8.c
@@ -21,7 +21,7 @@ INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_800121A0);
 INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_800121D4);
 
 bool func_80012214(void) {
-    D_800122A0->unk84 = (s32) D_800122A0->unk88;
+    D_800122A0->unk84 = (s32)D_800122A0->unk88;
     return true;
 }
 
