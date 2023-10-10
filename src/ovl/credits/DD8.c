2d0255561c0f6e2c607f16de0c3404d602b48381
DerpPrincess <AllisonMackenzieJohnson@gmail.com>
2023-10-09 21:46:49 -0500

    Credits

---
+++ src/ovl/credits/DD8.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80010DD8);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80011298);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80011590);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_800116A8);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_800116F4);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80011CAC);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80011D3C);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80012108);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_800121A0);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_800121D4);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80012214);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_80012234);
+
+INCLUDE_ASM("asm/ovl/credits/nonmatchings/DD8", func_8001225C);
