9b7983e3273c2b26896f81c9c26974b1b661a09b
DerpPrincess <AllisonMackenzieJohnson@gmail.com>
2023-10-12 07:43:58 -0500

    Gallow's Gauntlet

---
+++ src/ovl/gg/A4.c
@@ -0,0 +1,53 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800100A4);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800101E0);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010340);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800103E4);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010728);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010770);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010870);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800108CC);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010AA8);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010B44);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010BC8);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010C54);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010CEC);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010D2C);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80010E84);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80011090);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800110D8);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80011250);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800113D8);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80011418);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800114D4);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80011540);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800117B4);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800118B0);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_800118FC);
+
+INCLUDE_ASM("asm/ovl/gg/nonmatchings/A4", func_80011A04);
