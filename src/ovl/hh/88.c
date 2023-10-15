1a06697eff8bfa3939a3679db2c4702e3ae04e4e
DerpPrincess <AllisonMackenzieJohnson@gmail.com>
2023-10-15 11:11:40 -0500

    Hall of Heroes

---
+++ src/ovl/hh/88.c
@@ -0,0 +1,161 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80010088);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80010208);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800102BC);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800102E8);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800115B0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011894);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800119C0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011A68);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011D18);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011EE0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011F14);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011F48);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80011FF4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001206C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800121E0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012274);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012350);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800123C4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800124F4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012524);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012600);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012784);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800128D4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012BFC);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80012D68);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001301C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800130C4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800130FC);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013104);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013164);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013194);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800131E0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013374);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800133A0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800133D0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013408);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013444);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013500);
+
+void func_80013528(void) {
+}
+
+void func_80013530(void) {
+}
+
+void func_80013538(void) {
+}
+
+void func_80013540(void) {
+}
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013548);
+
+void func_8001360C(void) {
+}
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013614);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013678);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800136A4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800136D4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001370C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800137B4);
+
+void func_800137DC(void) {
+}
+
+void func_800137E4(void) {
+}
+
+void func_800137EC(void) {
+}
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800137F4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001381C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001399C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013A18);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013B7C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013C94);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013CD8);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013D04);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013D38);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013EE0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80013F44);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001414C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80014190);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800141B0);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80014214);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80014300);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80014368);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800143A8);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80014660);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800148DC);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_8001494C);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_800149F4);
+
+INCLUDE_ASM("asm/ovl/hh/nonmatchings/88", func_80014A38);
