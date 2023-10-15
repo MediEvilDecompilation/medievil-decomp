28c4c307c0684ba39faa2117450ad7747253329f
DerpPrincess <AllisonMackenzieJohnson@gmail.com>
2023-10-15 10:58:05 -0500

    The Entrance Hall

---
+++ src/ovl/eh/68.c
@@ -0,0 +1,49 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80010068);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800100E8);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800101DC);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800102E4);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_8001067C);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800107F4);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80010838);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800108AC);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80010AE8);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80010F34);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_8001107C);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800112EC);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011664);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_800117A8);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011954);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011A5C);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011AC0);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011B7C);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011BC8);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011C4C);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011D14);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011E58);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80011FA8);
+
+INCLUDE_ASM("asm/ovl/eh/nonmatchings/68", func_80012088);
